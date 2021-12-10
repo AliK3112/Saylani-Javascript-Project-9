aa0fad72b810fe0f4d8349ef88fbc5c751eec997
Ali Khalil <83224003+JanuMshkillz@users.noreply.github.com>
2021-12-10 20:47:21 +0500

    Preloaded Handled

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
+
 const vidContainer = document.querySelector(".video-container");
-vidContainer.playbackRate = 0.45;
+vidContainer.playbackRate = 0.45; // Making the video play slower
 const button = document.querySelector(".switch-btn");
 button.addEventListener("click", function () {
   if (!button.classList.contains("slide")) {
